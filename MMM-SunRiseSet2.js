84d199fe385d4fa56dfaa84a1d627b32af03b6ce
dathbe <github@beffa.us>
2024-03-05 16:23:57 -0800

    Update MMM-SunRiseSet2.js
    messing with images

--- MMM-SunRiseSet2.js
+++ MMM-SunRiseSet2.js
@@ -93,7 +93,7 @@ Module.register("MMM-SunRiseSet2", {
 		} else if (this.config.image == "map") {
 			var getTimeStamp = new Date().getTime(); // @Sean & @ Strawberry
 			//img.src = "http://api.usno.navy.mil/imagery/earth.png?date=today&seed=" + getTimeStamp; //
-			img.src = "https://www.timeanddate.com/scripts/sunmap.php?iso=20240306T0012&earth=1";
+			img.src = "https://beffalaw.com/graphics/beffalaw-banner.png";
 //			console.log(img.src);
 			pic.appendChild(img);
 			wrapper.appendChild(pic);
