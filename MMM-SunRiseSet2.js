4bf542a2e1177e4a9bb9245958797185d5dd88d7
dathbe <github@beffa.us>
2024-03-05 16:54:29 -0800

    Update MMM-SunRiseSet2.js

--- MMM-SunRiseSet2.js
+++ MMM-SunRiseSet2.js
@@ -93,14 +93,13 @@ Module.register("MMM-SunRiseSet2", {
 		} else if (this.config.image == "map") {
 			var getTimeStamp = new Date().getTime(); // @Sean & @ Strawberry
 			//img.src = "http://api.usno.navy.mil/imagery/earth.png?date=today&seed=" + getTimeStamp; //
-			img.src = "https://img.lemde.fr/2014/07/09/0/221/1160/580/2880/1440/45/0/ill_4454099_adf1_doge-image-en-tete.jpg";
+			img.src = "https://www.timeanddate.com/scripts/sunmap.php?iso=20240306T0012&earth=1";
 //			console.log(img.src);
 			pic.appendChild(img);
 			wrapper.appendChild(pic);
 		} else {
 			var getTimeStamp = new Date().getTime(); // @Sean & @ Strawberry
-			//img.src = "http://api.usno.navy.mil/imagery/earth.png?view=rise&seed=" + getTimeStamp; //
-			img.src = "https://www.timeanddate.com/scripts/sunmap.php?iso=20240306T0012&earth=1";
+			img.src = "http://api.usno.navy.mil/imagery/earth.png?view=rise&seed=" + getTimeStamp;
 			pic.appendChild(img);
 			wrapper.appendChild(pic);
 
@@ -208,15 +207,13 @@ Module.register("MMM-SunRiseSet2", {
 		} else {
 			var getTimeStamp = new Date().getTime(); // @Sean & @ Strawberry
 		if (this.config.dayOrNight == "day") {
-			//img.src = "http://api.usno.navy.mil/imagery/earth.png?view=rise&seed=" + getTimeStamp; //
-			img.src = "https://www.timeanddate.com/scripts/sunmap.php?iso=20240306T0012&earth=1";
+			img.src = "http://api.usno.navy.mil/imagery/earth.png?view=rise&seed=" + getTimeStamp; //
 			pic.appendChild(img);
 			wrapper.appendChild(pic);
 
 		} else {
 			var getTimeStamp = new Date().getTime(); // @Sean & @ Strawberry
-			//img.src = "http://api.usno.navy.mil/imagery/earth.png?view=set&seed=" + getTimeStamp;
-			img.src = "https://www.timeanddate.com/scripts/sunmap.php?iso=20240306T0012&earth=1";
+			img.src = "http://api.usno.navy.mil/imagery/earth.png?view=set&seed=" + getTimeStamp;
 			pic.appendChild(img);
 			wrapper.appendChild(pic);
 
